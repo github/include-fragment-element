648926e7913a0561b6ef05c024cbdb7341e43aae
Mu-An ✌️ Chiou <muan@github.com>
2017-12-20 13:45:23 +0800

    Allow promises for now

--- include-fragment-element.js
+++ include-fragment-element.js
@@ -1,3 +1,5 @@
+/* eslint-disable github/no-then */
+
 ;(function() {
   'use strict'
 
--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 /* eslint-env mocha */
+/* eslint-disable github/no-then */
 /* global assert */
 
 let count
