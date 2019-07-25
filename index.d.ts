1a38ebdedd30fc54f49adda5f2045c615e0cb462
Kristján Oddsson <koddsson@gmail.com>
2019-07-25 09:36:57 +0100

    fix getter by changing it to be `readonly`

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 export default class IncludeFragmentElement extends HTMLElement {
-  get data(): Promise<string>;
+  readonly data: Promise<string>;
   src: string;
   fetch(request: Request): Promise<Response>;
 }
